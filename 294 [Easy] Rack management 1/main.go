f2984fd56d85c03c8cd9b84c586792af3366a701
Gregorio Martinez <greg@gmartinez.com>
2016-12-05 22:10:42 -0500

    Add Solution for 294 Easy Bonus 1

--- 294 [Easy] Rack management 1/main.go
+++ 294 [Easy] Rack management 1/main.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println(scrabble("eerriin", "eerie"))
 	fmt.Println(scrabble("orrpgma", "program"))
 	fmt.Println(scrabble("orppgma", "program"))
+
+	fmt.Println(scrabble("pizza??", "pizzazz"))
+	fmt.Println(scrabble("piizza?", "pizzazz"))
+	fmt.Println(scrabble("a??????", "program"))
+	fmt.Println(scrabble("b??????", "program"))
 }
 
 func scrabble(tiles string, word string) bool {
@@ -25,7 +30,11 @@ func scrabble(tiles string, word string) bool {
 	for _, r := range word {
 		dict[r]--
 		if dict[r] < 0 {
-			return false
+			if dict['?'] > 0 {
+				dict['?']--
+			} else {
+				return false
+			}
 		}
 	}
 	return true
